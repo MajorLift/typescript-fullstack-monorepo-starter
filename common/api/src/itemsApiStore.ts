ebeaab8f12e5319fb7c7c6b6eab420414dbe482d
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2022-07-20 00:19:31 -0700

    set up rtk-query, write reducer endpoints for items

---
+++ common/api/src/itemsApiStore.ts
@@ -0,0 +1,96 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import type { Item, UUID } from '@mono/feature'
+
+export const itemsApi = createApi({
+  reducerPath: 'itemsApi',
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/api/v1/',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+  }),
+  tagTypes: ['Items'],
+
+  endpoints: (builder) => ({
+    findAllItems: builder.query<Item[], void>({
+      query: () => '/items',
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ _id }) => ({ type: 'Items' as const, _id })),
+              { type: 'Items', _id: 'ALL_LIST' },
+            ]
+          : [{ type: 'Items', _id: 'ALL_LIST' }],
+    }),
+
+    findItemById: builder.query<Item, UUID>({
+      query: (itemId) => `/items/${itemId}`,
+      providesTags: (result, error, _id) => [{ type: 'Items', _id }],
+    }),
+
+    findItemsByUser: builder.query<Item[], UUID>({
+      query: (userId) => `/users/${userId}/items`,
+      providesTags: (result, error, userId) =>
+        result
+          ? [
+              ...result.map(({ _id }) => ({
+                type: 'Items' as const,
+                userId,
+                _id,
+              })),
+              { type: 'Items', userId, _id: 'USER_LIST' },
+            ]
+          : [{ type: 'Items', userId, _id: 'USER_LIST' }],
+    }),
+
+    findItemByUser: builder.query<Item, { userId: UUID; itemId: UUID }>({
+      query: ({ userId, itemId }) => `/users/${userId}/items/${itemId}`,
+      providesTags: (result, error, { userId, itemId }) => [
+        { type: 'Items', userId, _id: itemId },
+      ],
+    }),
+
+    createItem: builder.mutation<Item, Partial<Item>>({
+      query: (body) => ({
+        url: '/items',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: [{ type: 'Items' }],
+    }),
+
+    updateItem: builder.mutation<Item, { itemId: UUID; body: Partial<Item> }>({
+      query: ({ itemId, body }) => ({
+        url: `/items/${itemId}`,
+        method: 'PUT',
+        body,
+      }),
+      invalidatesTags: (result, error, { itemId }) => [
+        { type: 'Items', _id: itemId },
+      ],
+    }),
+
+    deleteItem: builder.mutation<UUID, UUID>({
+      query: (itemId) => ({
+        url: `/items/${itemId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, itemId) => [
+        { type: 'Items', _id: itemId },
+      ],
+    }),
+  }),
+})
+
+export const {
+  useFindAllItemsQuery,
+  useFindItemByIdQuery,
+  useFindItemsByUserQuery,
+  useFindItemByUserQuery,
+  useCreateItemMutation,
+  useUpdateItemMutation,
+  useDeleteItemMutation,
+} = itemsApi
--- common/core/src/store.ts
+++ common/core/src/store.ts
@@ -1,8 +1,12 @@
+import { itemsApi } from '@mono/api'
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
+    [itemsApi.reducerPath]: itemsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(itemsApi.middleware),
 })
 
 export type AppDispatch = typeof store.dispatch

405480e25474873a23f2f282a44bcb9dda57fb14
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-04-04 04:30:59 -0700

    new message for empty data response

--- app/src/components/items/List.tsx
+++ app/src/components/items/List.tsx
@@ -14,8 +14,10 @@ export const List = (): JSX.Element => {
     <div className="text-gray-200">Error: {`${JSON.stringify(error)}`}</div>
   ) : isLoading ? (
     <div className="text-gray-200">Loading...</div>
-  ) : !data ? (
-    <div className="text-gray-200">Write your first note!</div>
+  ) : !data?.length ? (
+    <div className="text-gray-200">
+      Write your first note by clicking on the "New" button!
+    </div>
   ) : (
     <div className="bg-white shadow-lg shadow-black rounded-xl">
       {data.map((item) => (

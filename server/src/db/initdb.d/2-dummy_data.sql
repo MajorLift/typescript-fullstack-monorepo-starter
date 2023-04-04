a932ac8322f60da286c63f2a29291bf951fca823
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-04-04 00:27:29 -0700

    formatting

--- server/src/db/initdb.d/2-dummy_data.sql
+++ server/src/db/initdb.d/2-dummy_data.sql
@@ -1,6 +1,4 @@
 INSERT INTO items (title, content) 
-    VALUES ('', '');
-INSERT INTO items (title, content) 
     VALUES ('Sample Item 1',
     '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 

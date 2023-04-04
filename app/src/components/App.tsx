a8cedd03367beef1011e6446d508558e4955d371
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-04-04 00:09:26 -0700

    fix no default route issue

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router'
 
 import { AuthLayout } from 'components/auth'
 import { ItemsLayout } from 'components/items'
-
 import { Navbar } from './Navbar'
 
 export default function App(): JSX.Element {
@@ -11,9 +10,8 @@ export default function App(): JSX.Element {
     <div id="App">
       <Navbar />
       <Routes>
-        <Route path="/" />
         <Route path="/login" element={<AuthLayout />} />
-        <Route path="/items" element={<ItemsLayout />} />
+        <Route path="/*" element={<ItemsLayout />} />
       </Routes>
     </div>
   )
